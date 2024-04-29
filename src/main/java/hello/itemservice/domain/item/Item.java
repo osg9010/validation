e13518f7c9b462ecac42d39cc534216f07641149
osg9010 <osg3419010@naver.com>
2024-04-29 09:40:21 +0900

    수정에도 빈 밸리데이션 적용

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -18,7 +18,7 @@ public class Item {
     private String itemName;
 
     @NotNull
-    @Range(max = 1000, min = 1000000)
+    @Range(min = 1000, max = 1000000)
     private Integer price;
 
     @NotNull
